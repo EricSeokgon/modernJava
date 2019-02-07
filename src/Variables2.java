59f259592d2a50e785e8ce4a51b8d9826bd01511
hadesyi <leesk55@gmail.com>
2019-02-07 14:34:03 +0900

    Variables2

---
+++ src/Variables2.java
@@ -0,0 +1,14 @@
+public class Variables2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int addResult = 3 + 5;
+        System.out.println(addResult);
+
+        int subResult = 3 - 5;
+        int milRestult = 3 * 5;
+        double divResult = 3.0 / 5;
+        int modResult = 5 % 3;
+        System.out.println(modResult);
+    }
+}
