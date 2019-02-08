c513f720039577dabc0ee368f72e7abc10f5b533
hadesyi <leesk55@gmail.com>
2019-02-09 03:49:02 +0900

    variables

--- src/Variables.java
+++ src/Variables.java
@@ -2,7 +2,6 @@ public class Variables {
     public static void main(String[] args) {
         int myInt = 3;
         long myLong = 3_432_432_434_324_234_567l;
-
         Double myDouble = 3.445345345345345435345436454567475756756734;
         float myFloat = 3.12312312312344F;
 
