a8bac7b02aab2993c78ab1e5fbc6d22a1bbf5dd3
hadesyi <leesk55@gmail.com>
2019-02-25 12:49:18 +0900

    comparator 인터페이스 예제

---
+++ src/com/acompany/LengthComparator.java
@@ -0,0 +1,12 @@
+package com.acompany;
+
+class LengthComparator implements Comparable<String> {
+    public int compare(String first, String second) {
+        return first.length() - second.length();
+    }
+
+    @Override
+    public int compareTo(String o) {
+        return 0;
+    }
+}
