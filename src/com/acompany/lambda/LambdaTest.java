a4015aa2d3587f6850469d27910aafb7d57c0706
hadesyi <leesk55@gmail.com>
2019-02-26 10:28:24 +0900

    람다 테스트

---
+++ src/com/acompany/lambda/LambdaTest.java
@@ -0,0 +1,12 @@
+package com.acompany.lambda;
+
+public class LambdaTest {
+    public static void main(String[] args) {
+        String[] animals = {"cat", "hippo", "giraffe", "elephant", "monkey", ""};
+
+        for (String item : animals) {
+            System.out.println(item);
+        }
+
+    }
+}
