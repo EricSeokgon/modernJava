cb66534e10003d3b6487e77e0fd6462b1271489b
hadesyi <leesk55@gmail.com>
2019-02-26 10:37:59 +0900

    람다식으로 변경

--- src/com/acompany/lambda/LambdaTest.java
+++ src/com/acompany/lambda/LambdaTest.java
@@ -6,19 +6,13 @@ import java.util.Comparator;
 public class LambdaTest {
     public static void main(String[] args) {
         String[] animals = {"cat", "hippo", "giraffe", "elephant", "monkey", ""};
-        AnimalComparator comparator = new AnimalComparator();
-        Arrays.sort(animals, comparator);
+
+        Arrays.sort(animals, (o1, o2) -> o1.length() - o2.length());
+
         for (String item : animals) {
             System.out.println(item);
         }
 
-    }
-
-    static class AnimalComparator implements Comparator<String> {
-        @Override
-        public int compare(String o1, String o2) {
-            return o1.length() - o2.length();
-        }
     }
 
 }
