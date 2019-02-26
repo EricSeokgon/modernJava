75ab3c5fb64ba1153c9a6b12b72d427a2eba3550
hadesyi <leesk55@gmail.com>
2019-02-26 10:34:12 +0900

    function 파라메터 방식

--- src/com/acompany/lambda/LambdaTest.java
+++ src/com/acompany/lambda/LambdaTest.java
@@ -1,12 +1,24 @@
 package com.acompany.lambda;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class LambdaTest {
     public static void main(String[] args) {
         String[] animals = {"cat", "hippo", "giraffe", "elephant", "monkey", ""};
-
+        AnimalComparator comparator = new AnimalComparator();
+        Arrays.sort(animals, comparator);
         for (String item : animals) {
             System.out.println(item);
         }
 
     }
+
+    static class AnimalComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.length() - o2.length();
+        }
+    }
+
 }
