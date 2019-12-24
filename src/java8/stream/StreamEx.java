69e731c2a55351e8a936f1994a194e754969a276
hadesyi <leesk55@gmail.com>
2019-12-24 15:08:59 +0900

    비어 있는 스트림 생성

--- src/java8/stream/StreamEx.java
+++ src/java8/stream/StreamEx.java
@@ -1,10 +1,13 @@
 package java8.stream;
 
+import com.sun.xml.internal.ws.policy.EffectiveAlternativeSelector;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class StreamEx {
     public static void main(String[] args) {
@@ -64,5 +67,21 @@ public class StreamEx {
         Stream<String> stream1 = Arrays.stream(arr);
         Stream<String> streamOfArrayPart = Arrays.stream(arr, 1, 3);
 
+        List<String> list = Arrays.asList("a", "b", "c");
+        Stream<String> stream2 = list.stream();
+        Stream<String> parallelStream = list.parallelStream();
+
+    }
+
+    //컬렉션 스트림
+    public interface Collection<E> extends Iterable<E> {
+        default Stream<E> stream() {
+            return StreamSupport.stream(spliterator(), false);
+        }
+    }
+
+    //비어 있는 스트림
+    public Stream<String> streamOf(List<String> list) {
+        return list == null || list.isEmpty() ? Stream.empty() : list.stream();
     }
 }
