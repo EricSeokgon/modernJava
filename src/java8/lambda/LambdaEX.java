f1fb7d077a288329621b87e776aea8b50ec35607
hadesyi <leesk55@gmail.com>
2019-12-24 15:00:04 +0900

    람다식 문법

--- src/java8/lambda/LambdaEX.java
+++ src/java8/lambda/LambdaEX.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class LambdaEX {
     public static void main(String[] args) {
-
+        //람다식
         int[] arr = new int[5];
         Arrays.setAll(arr, i -> (int) (Math.random() * 5) + 1);
 
     }
 
+    //일반 메서드
     int method(int i) {
         return (int) (Math.random() * 5) + 1;
     }
