14e6611f35b6863ff2c600ba6308c50d6d0825e3
hadesyi <leesk55@gmail.com>
2019-12-24 14:21:28 +0900

    스트림의 연산

--- src/java8/stream/StreamEx.java
+++ src/java8/stream/StreamEx.java
@@ -15,8 +15,10 @@ public class StreamEx {
         Arrays.sort(strArr);
         Collections.sort(strList);
 
+
         for (String str : strArr) {
             System.out.println(str);
+
         }
         for (String str : strList) {
             System.out.println(str);
@@ -26,8 +28,21 @@ public class StreamEx {
         Stream<String> listStream = strList.stream();
         Stream<String> arrayStream = Arrays.stream(strArr);
 
+        System.out.println("===============");
         listStream.sorted().forEach(System.out::println);
+        System.out.println("===============");
         arrayStream.sorted().forEach(System.out::println);
+        System.out.println("===============");
+        //스트림 특징
+        //스트림은 일회용이다. 스트림은 한번 사용하면 닫혀서 다시 사용할수 없다.
+        //listStream.sorted().forEach(System.out::println);
+        // int numOfElement = listStream.count(); //스트림이 이미 닫힘
 
+        //스트림의 연산
+        Stream<String> stream = strList.stream();
+        stream.distinct()
+                .limit(5)
+                .sorted()
+                .forEach(System.out::println);
     }
 }
