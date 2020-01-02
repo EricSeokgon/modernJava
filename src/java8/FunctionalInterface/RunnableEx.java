f8bab2d08751bd2def6dab1bf9e0189e42f9fd13
hadesyi <leesk55@gmail.com>
2020-01-02 14:03:26 +0900

    Predicate<T> 하나의 인자와 리턴타입을 가진다.  반환타입은 voolean타입으로 고정

--- src/java8/FunctionalInterface/RunnableEx.java
+++ src/java8/FunctionalInterface/RunnableEx.java
@@ -3,6 +3,7 @@ package java8.FunctionalInterface;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class RunnableEx {
@@ -19,6 +20,8 @@ public class RunnableEx {
         Function<String, Integer> f = str -> Integer.parseInt(str);
         Integer apply = f.apply("1");
 
+        Predicate<String> p = str -> str.isEmpty();
+        final boolean hello = p.test("hello");
 
 
     }
