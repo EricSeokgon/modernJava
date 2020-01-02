575931a12b2bbb2a2f74b4db8123de2dc70bdbe8
hadesyi <leesk55@gmail.com>
2020-01-02 11:37:21 +0900

    filter 람다식 사용법

--- src/java8/stream/StreamEx2.java
+++ src/java8/stream/StreamEx2.java
@@ -2,6 +2,8 @@ package java8.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
 
 public class StreamEx2 {
     public static void main(String[] args) {
@@ -19,6 +21,15 @@ public class StreamEx2 {
         count = 0;
         count = names.stream().filter(x -> x.contains("o")).count();
         System.out.println("Count : " + count);
+
+        names.stream(); //Colltection에서 스트림 생성
+
+        Double[] dArra = {3.1, 3.2, 3.3};
+        Arrays.stream(dArra); //배열로 스트림 생성
+
+        //중개 연산
+        //Filter
+        Stream<String> a = names.stream().filter(x -> x.contains("o"));
 
     }
 }
