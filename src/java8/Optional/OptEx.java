b61d18191037d5277b7a03f981167d441a30b62e
hadesyi <leesk55@gmail.com>
2020-01-02 15:17:08 +0900

    optional 예제

--- src/java8/Optional/OptEx.java
+++ src/java8/Optional/OptEx.java
@@ -1,5 +1,7 @@
 package java8.Optional;
 
+import java.util.Optional;
+
 public class OptEx {
     public void method01(Object obj) {
         if (obj == null) {
@@ -12,10 +14,22 @@ public class OptEx {
         if (str == null) {
             //null을 그대로 리턴하는건 클라이언트에서 다시금 null체크를 해줘야하기 때무넹 기본값을 리턴하는걸 권한다.
             return null;
-            return "";
+            //return "";
         }
         //로직
         return "";
+    }
+
+    class Optonal<T> {
+        private T t;
+    }
+
+    public static void main(String[] args) {
+        String str = "hello";
+        Optional<String> str1 = Optional.of(str);
+        Optional<String> str2 = Optional.ofNullable(str);
+        Optional<Object> empty = Optional.empty();
+
     }
 
 }
