daafdfab8673e17eb398b2adb404c5fdfc7ef294
hadesyi <leesk55@gmail.com>
2020-01-02 13:30:02 +0900

    Supplier<T> 인자는 받지 않으며 리턴 타입만 존재하는 메서드를 갖고있다.

--- src/java8/FunctionalInterface/RunnableEx.java
+++ src/java8/FunctionalInterface/RunnableEx.java
@@ -1,9 +1,15 @@
 package java8.FunctionalInterface;
 
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
 public class RunnableEx {
     public static void main(String[] args) {
         Runnable r = () -> System.out.println("hello functional");
         r.run();
+
+        Supplier<String> s = () -> "hello supplier";
+        String result = s.get();
 
     }
 }
