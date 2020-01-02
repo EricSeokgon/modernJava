1645e48c46e087780711e04fceaffa6a7dd18278
hadesyi <leesk55@gmail.com>
2020-01-02 13:26:07 +0900

    람다 표현식 변경

--- src/java8/lambda/FruitEx.java
+++ src/java8/lambda/FruitEx.java
@@ -29,7 +29,7 @@ public class FruitEx {
     }
 
     static List<Fruit> extractFruitList(List<Fruit> fruits, Predicate<Fruit> predicate) {
-        List<Fruit> resultList = new ArrayList <>();
+        List<Fruit> resultList = new ArrayList<>();
         for (Fruit fruit : fruits) {
             if (predicate.test(fruit)) {
                 resultList.add(fruit);
@@ -37,6 +37,25 @@ public class FruitEx {
         }
         return resultList;
     }
+
+    List<Fruit> fruits = Arrays.asList(new Fruit("apple", "red"), new Fruit("melon", "green"), new Fruit("banana", "yellow"));
+    List<Fruit> appleList = extractFruitList(fruits, new Predicate<Fruit>() {
+        @Override
+        public boolean test(Fruit fruit) {
+            return "apple".equals(fruit.getName());
+        }
+    });
+
+    List<Fruit> reList = extractFruitList(fruits, new Predicate<Fruit>() {
+        @Override
+        public boolean test(Fruit fruit) {
+            return "red".equals(fruit.getColor());
+        }
+    });
+
+    List<Fruit> appleList1 = extractFruitList(fruits, fruit -> "apple".equals(fruit.getName()));
+    List<Fruit> redList1 = extractFruitList(fruits, fruit -> "red".equals(fruit.getColor()));
+
 
 }
 
