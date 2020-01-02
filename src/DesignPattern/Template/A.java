38ee0fdefe1445093eac9c295674d8b769910ff6
hadesyi <leesk55@gmail.com>
2020-01-02 17:05:38 +0900

    템플릿 메소드 예제

--- src/DesignPattern/Template/A.java
+++ src/DesignPattern/Template/A.java
@@ -4,8 +4,11 @@ abstract class A {
     public int templatemethod(int num1, int num2) { //재사용하고자하는 template 메서드
         System.out.println("입력받은 값 => [" + num1 + "," + num2 + "]");
         int result = calculation(num1, num2);
+        hook();
         return result;
     }
 
     public abstract int calculation(int num1, int num2); //오버라이딩에 의해 구현제가 바뀔수 있는 추상메서드
+
+    public void hook() { } //아무것도 하지않는 '구현' 메서드
 }
--- src/DesignPattern/Template/Multi.java
+++ src/DesignPattern/Template/Multi.java
@@ -5,4 +5,9 @@ public class Multi extends A {
     public int calculation(int num1, int num2) {
         return num1 * num2;
     }
+
+    @Override
+    public void hook() {
+        System.out.println("연산 종료"); //구현 메서드 이기때문에 필요에 따라 오버라이딩 하면된다.
+    }
 }
