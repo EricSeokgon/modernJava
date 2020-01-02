2070a519d253e68df51c2d0643a0e096a5244f0e
hadesyi <leesk55@gmail.com>
2020-01-02 15:35:49 +0900

    optional 활용

--- src/java8/Optional/OptEx.java
+++ src/java8/Optional/OptEx.java
@@ -2,6 +2,7 @@ package java8.Optional;
 
 import java.util.Optional;
 
+
 public class OptEx {
     public void method01(Object obj) {
         if (obj == null) {
@@ -26,9 +27,28 @@ public class OptEx {
 
     public static void main(String[] args) {
         String str = "hello";
-        Optional<String> str1 = Optional.of(str);
-        Optional<String> str2 = Optional.ofNullable(str);
-        Optional<Object> empty = Optional.empty();
+        Optional<String> str1 = Optional.of(str); // str이 null이면 NPE발생
+        Optional<String> str2 = Optional.ofNullable(str); //str이 null 이면 빈 Optinal 객체 반환
+        Optional<Object> empty = Optional.empty(); // 빈 Optional 객체 반환
+
+        Optional<Object> empty1 = Optional.empty();
+        Object o = empty1.orElse(new Object());
+        Object o1 = empty1.orElseGet(() -> new Object());
+        Object o2 = empty1.orElseGet(Object::new);
+
+        Object object2 = empty1.orElseGet(() -> {
+            Object obj = new Object();
+            //obj.setitem();
+            //obj.setName();
+        });
+
+        Optional<Integer> integerOptional = Optional.empty();
+        Integer integer = integerOptional.orElse(5);
+        Integer integer1 = integerOptional.orElseGet(() -> 5);
+
+        Optional<Object> empty2 = Optional.empty();
+        Object obj = empty2.map(Object::getClass)
+                .orElse(null);
 
     }
 
