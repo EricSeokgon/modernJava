4fd84d9959cd54986f5c557dd0c90c77a285edcb
hadesyi <leesk55@gmail.com>
2020-01-08 16:57:04 +0900

    왜 순서를 고려해야 하는가?

--- src/java8/stream/StreamTutorial.java
+++ src/java8/stream/StreamTutorial.java
@@ -75,6 +75,49 @@ public class StreamTutorial {
                 .forEach(s -> System.out.println("forEach: " + s));
 
 
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .filter(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                })
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+
     }
 
 }
