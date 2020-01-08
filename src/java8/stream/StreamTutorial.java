55fae2a7da4b7446bd86e5318ab27c6b046e2f0e
hadesyi <leesk55@gmail.com>
2020-01-08 16:51:13 +0900

    stream 연산 가공 순서

--- src/java8/stream/StreamTutorial.java
+++ src/java8/stream/StreamTutorial.java
@@ -50,7 +50,21 @@ public class StreamTutorial {
                 .filter(s -> {
                     System.out.println("fiter: " + s);
                     return true;
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .anyMatch(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
                 });
+
+
 
 
     }
