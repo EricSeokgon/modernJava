e63c7994feb5d6faceecb73b8493a07a9b3ec180
hadesyi <leesk55@gmail.com>
2020-01-08 16:45:36 +0900

    여러가지 스트림 예제

--- src/java8/stream/StreamTutorial.java
+++ src/java8/stream/StreamTutorial.java
@@ -2,6 +2,7 @@ package java8.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamTutorial {
@@ -21,6 +22,30 @@ public class StreamTutorial {
         Stream.of("a1", "a2", "a3")
                 .findFirst()
                 .ifPresent(System.out::println);
+
+        IntStream.range(1, 4)
+                .forEach(System.out::println);
+
+        Arrays.stream(new int[]{1, 2, 3})
+                .map(n -> 2 * n + 1)
+                .average()
+                .ifPresent(System.out::println);
+
+        Stream.of("a1", "a2", "a3")
+                .map(s -> s.substring(1))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .ifPresent(System.out::println);
+
+        IntStream.range(1, 4)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+        Stream.of(1.0, 2.0, 3.0)
+                .mapToInt(Double::intValue)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
 
 
     }
