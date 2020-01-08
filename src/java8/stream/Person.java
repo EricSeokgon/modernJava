06e6bffb805cc0e51a735153b27341a630b70bce
hadesyi <leesk55@gmail.com>
2020-01-08 17:01:57 +0900

    Person 클래스 생성

---
+++ src/java8/stream/Person.java
@@ -0,0 +1,17 @@
+package java8.stream;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
