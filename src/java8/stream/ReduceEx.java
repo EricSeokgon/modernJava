21f9c3b5c992eb4618f2355a393c8c527de3a9f9
hadesyi <leesk55@gmail.com>
2020-01-13 15:34:17 +0900

    병렬스트림 예제

--- src/java8/stream/ReduceEx.java
+++ src/java8/stream/ReduceEx.java
@@ -1,5 +1,8 @@
 package java8.stream;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -61,6 +64,10 @@ public class ReduceEx {
                 .map(s -> {
                     System.out.format("map: %s [%s]\n", s, Thread.currentThread().getName());
                     return s.toUpperCase();
+                })
+                .sorted((s1, s2) -> {
+                    System.out.format("sort: %s <> %s[%s]\n", s1, s2, Thread.currentThread().getName());
+                    return s1.compareTo(s2);
                 })
                 .forEach(s -> System.out.format("forEach : %s [%s]\n", s, Thread.currentThread().getName()));
 
